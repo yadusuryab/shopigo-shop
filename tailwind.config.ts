9416694965ec703d8faa2d2c2122b1b4fba5c355
Travis CI <yadusurya666@gmail.com>
2025-07-09 21:55:24 +0530

    Tailwind Config Error Fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,19 +10,7 @@ const config: Config = withUt({
   ],
   theme: {
   	extend: {
-  		'keyframes': {
-  			'spinner-leaf-fade': {
-  				'0%, 100%': {
-  					'opacity': '0'
-  				},
-  				'50%': {
-  					'opacity': '1'
-  				}
-  			}
-  		},
-  		'animation': {
-  			'spinner-leaf-fade': 'spinner-leaf-fade 800ms linear infinite'
-  		},
+  		
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
